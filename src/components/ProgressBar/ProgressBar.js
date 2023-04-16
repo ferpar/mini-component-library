cf5089259a26e0fb64b57a578f1654edb9bb9103
fer <ferp@protonmail.ch>
2023-04-16 09:49:37 +0200

    simplified interface for progress bar component

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 import { COLORS } from "../../constants";
 import VisuallyHidden from "../VisuallyHidden";
 
-const ProgressBar = ({ value, size, width = 372 }) => {
+const ProgressBar = ({ value, size }) => {
   return (
     <Wrapper>
       <VisuallyHidden>{value}%</VisuallyHidden>
@@ -17,11 +17,10 @@ const ProgressBar = ({ value, size, width = 372 }) => {
         aria-valuenow={value}
         aria-valuemin="0"
         aria-valuemax="100"
-        width={width}
       >
-        <ProgressBarSvg width={width} size={size}>
+        <ProgressBarSvg size={size}>
           <Rail size={size} />
-          <Fill width={width} value={value} size={size} />
+          <Fill value={value} size={size} />
         </ProgressBarSvg>
       </ProgressBarSpan>
     </Wrapper>
@@ -30,30 +29,37 @@ const ProgressBar = ({ value, size, width = 372 }) => {
 
 const Wrapper = styled.div`
   position: relative;
+  width: 100%;
 `;
 
 const ValueSpan = styled.span`
   position: relative;
+  display: inline-block;
+  width: 10%;
   top: ${({ size }) => (size === "large" ? "5px" : "0")}};
 `;
 
 const ProgressBarSpan = styled.span`
+  display: inline-block;
   position: absolute;
   top: 0;
-  left: 100px;
+  left: 10%;
   display: inline-block;
-  width: ${({ width }) => width}px;
+  // width: ${({ width }) => width}px;
+  width: 90%;
 `;
 
 const ProgressBarSvg = styled.svg`
-  width: ${({ width }) => width}px;
-  // height: 24px;
+  // width: ${({ width }) => width}px;
+  display: inline-block;
+  width: 100%;
   height: ${({ size }) =>
     size === "large" ? "24px" : size === "small" ? "8px" : "12px"};
 `;
 
 const Rail = styled.rect`
-  width: inherit;
+  display: inline-block;
+  width: 100%;
   height: 100%;
   fill: ${COLORS.transparentGray15};
   rx: 4px;
@@ -64,14 +70,13 @@ const Rail = styled.rect`
 
 const Fill = styled.rect`
   fill: ${COLORS.primary};
-  clip-path: ${(props) =>
-    `inset( 0 ${((100 - props.value) * props.width) / 100}px 0 0)`};
+  clip-path: ${(props) => `inset( 0 ${100 - props.value}% 0 0)`};
   rx: 4px;
   ry: 4px;
   transform: ${({ size }) => size === "large" && `translate(4px, 4px)`};
   height: ${({ size }) =>
     size === "large" ? "16px" : size === "small" ? "8px" : "12px"};
-  width: ${({ width }) => width - 8}px;
+  width: 100%;
 `;
 
 export default ProgressBar;
